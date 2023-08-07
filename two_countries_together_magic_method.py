c157a24ad64503e6a263fb714fdf9dff52dc516e
olyonka <olena.mykhailova@gmail.com>
2023-08-07 19:16:36 +0300

    Add files via upload

---
+++ two_countries_together_magic_method.py
@@ -0,0 +1,19 @@
+class Country:
+    def __init__(self, name, population):
+        self.name = name
+        self.population = population
+
+    def __add__(self, other_country):
+        new_name = f"{self.name} {other_country.name}"
+        new_population = self.population + other_country.population
+        return Country(new_name, new_population)
+
+
+# Example usage
+bosnia = Country('Bosnia', 10_000_000)
+herzegovina = Country('Herzegovina', 5_000_000)
+
+bosnia_herzegovina = bosnia + herzegovina
+
+print(bosnia_herzegovina.population)  # Output: 15_000_000
+print(bosnia_herzegovina.name)  # Output: 'Bosnia Herzegovina'
